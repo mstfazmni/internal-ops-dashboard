a5f598448ad280f4ea371f9d929b75f907ea664e
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2026-01-12 14:49:54 -0700

    some small fixes

--- internal-ops-dashboard/src/app/App.tsx
+++ internal-ops-dashboard/src/app/App.tsx
@@ -1,11 +1,12 @@
-import { CustomerSummaryHeader } from '../components/customer/CustomerSummaryHeader'
-import '../App.css'
+import { CustomerSummaryHeader } from '../components/customer/CustomerSummaryHeader';
+import { mockCustomerSummary } from '../mock/customerSummary.mock';
+import '../App.css';
 
 function App() {
 
   return (
     <>
-      <CustomerSummaryHeader />
+      <CustomerSummaryHeader customer={mockCustomerSummary}></CustomerSummaryHeader>
     </>
   )
 }
--- internal-ops-dashboard/src/components/customer/CustomerSummaryHeader.tsx
+++ internal-ops-dashboard/src/components/customer/CustomerSummaryHeader.tsx
@@ -1,22 +1,28 @@
-import {mockCustomerSummary} from '../../mock/customerSummary.mock';
+import type { CustomerSummary } from "../../types/customer";
 
-export function CustomerSummaryHeader () {
-    // side-effect / debug logging should happen outside JSX
-    console.log(mockCustomerSummary.id);
+interface Props {
+    customer: CustomerSummary
+}
+export function CustomerSummaryHeader ({customer}: Props) {
 
     return (
-        <>
+        <header>
             <div>
-                <ul key={mockCustomerSummary.id}>
-                    <li>{mockCustomerSummary.name}</li>
-                    <li>{mockCustomerSummary.email}</li>
-                    <li>{mockCustomerSummary.status}</li>
-                    <li>{mockCustomerSummary.riskStatus}</li>
-                    <li>{mockCustomerSummary.flagsCount}</li>
-                    <li>{mockCustomerSummary.notesCount}</li>
-                    <li>{mockCustomerSummary.createdAt}</li>
-                </ul>
+                <h1>{customer.name}</h1>
+                <p>{customer.email}</p>
             </div>
-        </>
+
+            <div>
+                <span data-role="status">Status: {customer.status}</span>
+                <span data-role="risk">Risk: {customer.riskStatus}</span>
+            </div>
+
+            <div>
+                <span data-role="flags">Flags: {customer.flagsCount}</span>
+                <span data-role="notes">Notes: {customer.notesCount}</span>
+            </div>
+
+            <time dateTime={customer.createdAt}>Created: {customer.createdAt}</time>
+        </header>       
     );
 }
