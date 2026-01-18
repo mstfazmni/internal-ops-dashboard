052fc6d673fbc8b26fde1def28fe8316065369d9
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2026-01-17 17:19:13 -0700

    create an array of objects to create 25 and 15 test transactions so i could test my pagination in seed.ts file and ran it again

--- internal-ops-dashboard/backend/prisma/seed.ts
+++ internal-ops-dashboard/backend/prisma/seed.ts
@@ -1,6 +1,11 @@
 import { prisma } from "../src/prisma";
 
-// main function to seed the database
+//  main function to seed the database
+//  seed.ts is not app code
+//  It’s a one-time script to populate the database
+//  You run it manually to create fake but realistic data
+//  That’s how everyone tests pagination. No UI. No Postman magic.
+
 async function main() {
     // create customer
     const customer = await prisma.customer.create({
@@ -30,33 +35,32 @@ async function main() {
         },
     });
 
-    // create transactions for account1
+    // create transactions for account1 25 rows to test pagination
+    // 1: This creates an array with 25 empty slots.
+    // 2: Now you loop through those 25 empty slots.
+    // 3: This line alternates the amount:
+        // If the index is even → -50
+        // If the index is odd → 100
+    // This code generates 25 fake transactions (alternating amounts) for one account and inserts them into the database so you can test pagination.
+    const transactionForAccount1 = Array.from({ length: 25 }).map((_, i) => ({
+        amount: i % 2 === 0 ? -50 : 100,
+        accountId: account1.id,
+    }));
+    
     await prisma.transaction.createMany({
-        data: [
-            {
-                amount: -200,
-                accountId: account1.id,
-            },
-             {
-                amount: 500,
-                accountId: account1.id,
-            },
-        ],
+        data: transactionForAccount1,
     });
 
-    // create transactions for account2
+    // create transactions for account2 15 rows to test pagination
+    const transactionsForAccount2 = Array.from({ length: 15 }).map((_, i) => ({
+        amount: i % 2 === 0 ? -20 : 40,
+        accountId: account2.id,
+    }));
+
     await prisma.transaction.createMany({
-        data: [
-            {
-                amount: -50,
-                accountId: account2.id,
-            },
-            {
-                amount: -100,
-                accountId: account2.id,
-            },
-        ],
+        data: transactionsForAccount2,
     });
+
 
     console.log("Seed data created successfully.");
 }
