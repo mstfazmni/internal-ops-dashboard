0888a1d2587e14d3c230458932ee07672add612d
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2026-02-03 16:29:30 -0700

    created transactions.api file to fetch trans

---
+++ internal-ops-dashboard/frontend/src/api/transactions.api.ts
@@ -0,0 +1,11 @@
+export async function fetchAccountTransactions (accountId: string) {
+    const response = await fetch(
+        `http://localhost:3000/accounts/${accountId}/transactions`
+    );
+
+    if(!response.ok) {
+        throw new Error("Failed to fetch transactions for account")
+    }
+
+    return response.json();
+}
