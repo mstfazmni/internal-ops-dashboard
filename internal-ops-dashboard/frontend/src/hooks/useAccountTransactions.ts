cf2a54d965eee7576b60db5ca2fb6bfd338f0caf
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2026-02-10 21:45:25 -0700

    clear error state when load(). Reeset page to 1 when accountId changes, so that we don't end up on a page that doesn't exist for the new account

--- internal-ops-dashboard/frontend/src/hooks/useAccountTransactions.ts
+++ internal-ops-dashboard/frontend/src/hooks/useAccountTransactions.ts
@@ -3,7 +3,7 @@ import type { Transaction } from "../types/transaction";
 import { fetchAccountTransactions } from "../api/transactions.api";
 
 export function useAccountTransactions (accountId: string) {
-    // const [data, setData] = useState<Transaction[]>([]);
+    
     const [transactions, setTransactions] = useState<Transaction[]>([]);
     const [totalPages, setTotalPages] = useState(1);
     const [loading, setLoading] = useState(true);
@@ -12,15 +12,22 @@ export function useAccountTransactions (accountId: string) {
     
     const limit = 5;
 
+    // Reeset page to 1 when accountId changes, so that we don't end up on a page that doesn't exist for the new account
+    useEffect(() => {
+        setPage(1);
+    }, [accountId]);
+
     useEffect(() => {
         async function load () {
             try {
                 setLoading(true);
+                setError(null);
+
                 const result = await fetchAccountTransactions(accountId, page, limit);
                 // console.log(result)
 
                 setTransactions(result.data);
-                setTotalPages(result.totalPages);
+                setTotalPages(result.totalPages || 1);
             } catch (err) {
                 setError("Could not load transactions.")
             } finally {
